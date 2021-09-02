98b90775a09065d7479a899c7b256f5a382a7164
Chuck Staples <cjstaples@gmail.com>
2021-09-02 19:18:30 -0400

    wip tests

--- test_workshop.py
+++ test_workshop.py
@@ -5,18 +5,12 @@ import unittest
 import workshop
 
 
-class MyTestCase(unittest.TestCase):
-    # def test_something(self):
-    #     self.assertEqual(True, False)  # add assertion here
+class WorkshopTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, True)  # add assertion here
 
-    def test_help_output_is_generated(capsys):
-        sys.argv = ['workshop.py', '-h']
-        with pytest.raises(SystemExit):
-            workshop.main()
-        capture = capsys.readouterr()
-        assert 'usage:' in capture.out
-        # assert 'Query and Update Rally project data' in out
-        # assert '-h, --help            show this help message and exit' in out
+    def test_something_false(self):
+        self.assertEqual(True, False)  # add assertion here
 
 
 if __name__ == '__main__':
