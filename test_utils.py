45c7b1cb063e39b21996f3535fc2ea6fe9a4d616
Chuck Staples <cjstaples@gmail.com>
2021-09-10 17:43:34 -0400

    more unit tests, no utils getters yet

--- test_utils.py
+++ test_utils.py
@@ -18,3 +18,21 @@ def test_get_test_true():
     args = argparse.Namespace(limit=99, test=True)
     test = utils.get_test(args)
     assert test is True
+
+
+def test_get_project():
+    args = argparse.Namespace(rally_project='Project Starlink')
+    project = utils.get_project(args)
+    assert project == 'Project Starlink'
+
+
+def test_get_sitename():
+    args = argparse.Namespace(sitename='sandbox_sitename_replace')
+    sitename = utils.get_sitename(args)
+    assert sitename == 'something_other_than_this'
+
+
+def test_get_workspace():
+    args = argparse.Namespace(rally_workspace='2021')
+    workspace = utils.get_workspace(args)
+    assert workspace == '2021'
